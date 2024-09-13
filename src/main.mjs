50eec971e5e5a705c4a4ed2666b21960387f3743
Maksim Ivanov <vonavimax@gmail.com>
2024-09-13 13:03:49 +0700

    feat: output comment

--- src/main.mjs
+++ src/main.mjs
@@ -1,23 +1,42 @@
 import * as core from '@actions/core'
+import * as github from '@actions/github'
 import { calcTechDept } from './calc-tech-dept.mjs'
-// import { createOctokit } from './octokit.mjs'
+import {
+  createOctokit,
+  createComment,
+  findAndUpdateComment
+} from './octokit.mjs'
 
 /**
  * The main function for the action.
  * @returns {Promise<void>} Resolves when the action is complete.
  */
 export async function run() {
-  // const oktokit = createOctokit()
-
+  const octokit = createOctokit()
   try {
-    const result = await calcTechDept()
+    const techDept = await calcTechDept()
 
-    // output result in table format to github comment
-    const table = Object.entries(result)
+    // output result in table format to pull request comment
+    const tableContent = Object.entries(techDept)
       .map(([key, value]) => `| ${key} | ${value} |`)
       .join('\n')
-    const comment = `## Tech Debt\n\n${table}`
-    core.setOutput('tech-debt', comment)
+
+    const commentTitle = '### Tech Debt'
+    const comment = `${commentTitle}\n\n| type | count |\n|---|---|\n${tableContent}`
+    core.debug(comment)
+
+    const updateResult = await findAndUpdateComment(
+      octokit,
+      github.context,
+      commentTitle,
+      comment
+    )
+    if (updateResult) {
+      core.debug('Updated the existing comment')
+      return
+    }
+
+    await createComment(octokit, github.context, comment)
   } catch (error) {
     // Fail the workflow run if an error occurs
     core.setFailed(error.message)
